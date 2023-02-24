1d9b37a824cf84c86a0cfd2e3c6bc60736641e7e
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-02-24 21:02:36 +0530

    image gen

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -42,6 +42,24 @@ export default async function (req, res) {
     });
     return;
   }
+  const dreamOptimize = req.body.dreamOptimize || '';
+  if (dreamOptimize.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid dream Optimize",
+      }
+    });
+    return;
+  }
+  const dreamImageDescription = req.body.dreamImageDescription || '';
+  if (dreamImageDescription.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid dream Image Description",
+      }
+    });
+    return;
+  }
 
   try {
     const completion = await openai.createCompletion({
@@ -50,7 +68,14 @@ export default async function (req, res) {
       temperature: 0.6,
       max_tokens:3500
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
+
+    const imageGen = await openai.createImage({
+      prompt: `${dreamImageDescription}+${dreamOptimize}`,
+      n: 1,
+      size: "1024x1024",
+    });
+
+    res.status(200).json({ result: completion.data.choices[0].text,url:imageGen.data.data[0].url });
     console.log(res)
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,10 @@ export default function Home() {
   const [dreamAnalysisDescriptionInput,setDreamAnalysisDescription] = useState("");
   const [dreamFeelingInput, setDreamFeeling] = useState("");
   const [walkingInfluenceInput, setWalkingInfluence] = useState("");
+  const [dreamOptimizeInput,setDreamOptimize] = useState("");
+  const [dreamImageDescriptionInput,setDreamImageDescription] = useState("");
   const [result, setResult] = useState();
+  const [url,setUrl] = useState("")
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -16,7 +19,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({dreamAnalysisDescription:dreamAnalysisDescriptionInput,dreamFeeling:dreamFeelingInput,walkingInfluence:walkingInfluenceInput }),
+        body: JSON.stringify({dreamAnalysisDescription:dreamAnalysisDescriptionInput,dreamFeeling:dreamFeelingInput,walkingInfluence:walkingInfluenceInput,dreamOptimize:dreamOptimizeInput,dreamImageDescription:dreamAnalysisDescriptionInput }),
       });
 
       const data = await response.json();
@@ -25,10 +28,14 @@ export default function Home() {
       }
 
       setResult(data.result);
+      setUrl(data.url);
       console.log(data)
+      
       setDreamAnalysisDescription("");
       setDreamFeeling("");
       setWalkingInfluence("");
+      setDreamImageDescription("");
+      setDreamOptimize("")
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -68,9 +75,26 @@ export default function Home() {
             value={walkingInfluenceInput}
             onChange={(e) => setWalkingInfluence(e.target.value)}
           />
+          <input
+            type="text"
+            name="dreamImageDescription"
+            placeholder="Describe the most memorable moment of your dream to me"
+            value={dreamImageDescriptionInput}
+            onChange={(e) => setDreamImageDescription(e.target.value)}
+          />
+          <input
+            type="text"
+            name="dreamOptimize"
+            placeholder="In what style of art would you like your dream to reflect?"
+            value={dreamOptimizeInput}
+            onChange={(e) => setDreamOptimize(e.target.value)}
+          />
           <input type="submit" value="Generate Dream" />
         </form>
         <div className={styles.result}>{result}</div>
+        <div className={styles.images}>
+          <img src={url}/>
+        </div>
       </main>
     </div>
   );
